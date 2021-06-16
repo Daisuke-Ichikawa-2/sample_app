18143ec02b89c51cbb1a88fc7c0d83fb24a28004
Daisuke Ichikawa <ichikawa_daisuke2@gmail.com>
2021-06-17 06:24:49 +0900

    ログイン機能の実装
    sessionモデルの作成、コントローラ 実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }   #大文字で入力されても小文字で保存する
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true,
-            length: { maximum: 255 },
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: true
-  has_secure_password #ハッシュ化した文字列で保存、2つの仮装属性使用、authenticateメソッド
+  has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+# 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+               BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
